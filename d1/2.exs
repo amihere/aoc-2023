800459ed9a993488a2296aec29a968e204058ae9
workkwaw <86957130+workkwaw@users.noreply.github.com>
2024-02-06 22:33:12 +0000

    finished d1p2

--- d1/2.exs
+++ d1/2.exs
@@ -22,7 +22,7 @@ defmodule DayOne do
     String.first(first) <> String.first(second)
   end
 
-  def replace_with_digit("", result) do
+  def replace_with_digit(line, result) when line == "" do
     result
   end
 
@@ -40,8 +40,12 @@ defmodule DayOne do
 
       [h | _] ->
         new_val = number_list()[h]
-        new_line = String.replace(line, h, new_val)
-        replace_with_digit(String.slice(new_line, 1..-1), String.replace(result, h, new_val))
+        new_line = String.replace(line, String.slice(h, 0..-2), new_val)
+
+        replace_with_digit(
+          String.slice(new_line, 1..-1),
+          String.replace(result, String.slice(h, 0..-2), new_val)
+        )
     end
   end
 
@@ -61,6 +65,7 @@ defmodule DayOne do
 
   def run do
     readlines("input.txt")
+    |> Enum.map(&replace_with_digit(&1, &1))
     |> Enum.map(&helper(&1))
     |> Enum.reduce(fn x, acc -> x + acc end)
     |> IO.puts()
