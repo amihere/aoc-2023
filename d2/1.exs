64586f35ce9275bd88363fb105a765581094d3fa
workkwaw <86957130+workkwaw@users.noreply.github.com>
2024-02-07 02:23:40 +0000

    finished d2p1

--- d2/1.exs
+++ d2/1.exs
@@ -1,4 +1,4 @@
-defmodule DayOne do
+defmodule DayTwo do
   def readlines(filepath) do
     case File.read(filepath) do
       {:ok, contents} ->
@@ -7,27 +7,50 @@ defmodule DayOne do
     end
   end
 
-  def helper(line) do
-    Regex.replace(~r/\D/, line, "") |> addFirstAndLast |> String.to_integer
+  def config() do
+    %{r: 12, g: 13, b: 14}
   end
 
-  def addFirstAndLast(line) do
-    case String.length(line) do
-      0 -> "0"
-      _ -> adder(line, String.reverse(line))
+  def create_game(line) when line != "" do
+    [id | game] = String.split(line, ":")
+    id = String.replace_prefix(id, "Game ", "") |> String.to_integer()
+    config = config()
+
+    [game | _] = game
+    invalid = String.split(game, ";") |> Enum.any?(&check_number(&1, config))
+
+    case invalid do
+      false -> id
+      _ -> 0
     end
   end
 
-  def adder(first, second) do
-    String.first(first) <> String.first(second)
+  def create_game(_) do
+    0
+  end
+
+  def check_number(game, config) do
+    colors = String.split(game, ",")
+
+    Enum.any?(colors, fn color ->
+      [number | rest] = String.trim(color) |> String.split(" ")
+      number = String.to_integer(number)
+      rest = List.first(rest) |> String.first()
+
+      case rest do
+        "g" -> number > config.g
+        "r" -> number > config.r
+        "b" -> number > config.b
+      end
+    end)
   end
 
   def run do
     readlines("input.txt")
-    |> Enum.map(&helper(&1))
-    |> Enum.reduce(fn x, acc -> x + acc end)
+    |> Enum.map(&create_game(&1))
+    |> Enum.sum()
     |> IO.puts()
   end
 end
 
-DayOne.run()
+DayTwo.run()
